95a27c42b47fd60fe7192a48491fe20f148fd69c
victorskl <sanl1@student.unimelb.edu.au>
2016-10-17 19:10:54 +1100

    Commit merge conflict

--- strike-server/src/main/java/strike/model/Protocol.java
+++ strike-server/src/main/java/strike/model/Protocol.java
@@ -5,6 +5,7 @@ public enum Protocol {
     serverid, locked, approved, roomchange, former, roomid, releaseidentity,
     list, quit, who, createroom, roomlist, rooms, roomcontents, identities, owner,
     lockroomid, releaseroomid, deleteroom, message, content, join, route, host, port,
-    movejoin, serverchange, listserver, serverlist, servers, address
+    movejoin, serverchange, listserver, serverlist, servers, address,
+    authenticate, username, password, rememberme, authresponse, success, reason
     ;
 }
