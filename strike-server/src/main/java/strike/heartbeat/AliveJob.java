2ba737418b3a2339dac2714d2bc01145d7cf0afd
Gayashan Amarasinghe <gayashan.amarasinghe@gmail.com>
2017-05-09 18:26:17 +1000

    Updated AliveJob to initiate an election when it detects the current coordinator has died.

--- strike-server/src/main/java/strike/heartbeat/AliveJob.java
+++ strike-server/src/main/java/strike/heartbeat/AliveJob.java
@@ -2,27 +2,30 @@ package strike.heartbeat;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 import strike.model.ServerInfo;
+import strike.service.BullyElectionManagementService;
 import strike.service.JSONMessageBuilder;
 import strike.service.PeerClient;
 import strike.service.ServerState;
 
 public class AliveJob implements Job {
 
-    private PeerClient peerClient = new PeerClient();
-    private JSONMessageBuilder messageBuilder = JSONMessageBuilder.getInstance();
-    private ServerState serverState = ServerState.getInstance();
+    private final PeerClient peerClient = new PeerClient();
+    private final JSONMessageBuilder messageBuilder = JSONMessageBuilder.getInstance();
+    private final ServerState serverState = ServerState.getInstance();
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
 
+        if (null != serverState.getCoordinator()) {
+            logger.debug("Current coordinator is : " + serverState.getCoordinator().getServerId());
+        }
+
         for (ServerInfo serverInfo : serverState.getServerInfoList()) {
             String serverId = serverInfo.getServerId();
-            if (serverId.equalsIgnoreCase(serverState.getServerInfo().getServerId())) {
+            String myServerId = serverState.getServerInfo().getServerId();
+            if (serverId.equalsIgnoreCase(myServerId)) {
                 continue;
             }
 
@@ -39,7 +42,19 @@ public class AliveJob implements Job {
                 String aliveErrorFactor = dataMap.get("aliveErrorFactor").toString();
 
                 count = serverState.getAliveMap().get(serverId);
+
                 if (count > Integer.parseInt(aliveErrorFactor)) {
+                    // if the offline server is the coordinator, start the election process
+                    if (null != serverState.getCoordinator() && serverInfo.getServerId().equalsIgnoreCase(serverState
+                            .getCoordinator().getServerId())) {
+                        // send the start election message to every server with a higher priority
+                        try {
+                            new BullyElectionManagementService().startElection(serverState.getServerInfo(), serverState
+                                    .getCandidateServerInfoList());
+                        } catch (SchedulerException e) {
+                            logger.error("Unable to start the election : " + e.getLocalizedMessage());
+                        }
+                    }
                     peerClient.relayPeers(messageBuilder.notifyServerDownMessage(serverId));
                     logger.debug("Notify server " + serverId + " down. Removing...");
 
